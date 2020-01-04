0fe70438214ba92d2c523ee1c2c5db08d8a02205
Hugo <h.dunogeant@protonmail.com>
2020-01-04 20:36:54 +0100

    fix

--- model/PostManager.php
+++ model/PostManager.php
@@ -8,7 +8,7 @@ class PostManager {
     public function getPosts() {
         $db = $this->dbConnect();
         $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
-        while ($data = $req->fetch()) {
+        while ($data = $req->fetch()) { //while loop for view 
             echo '<div style="border: 2px solid black;"><h1>' . $data['title'] . '</h1>' . $data['content'] . '<br />' . $data['creation_date_fr'] . '</div>';
         }
     }
