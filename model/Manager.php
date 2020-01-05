6eb4591a62123db43f85b8518d8b8eb4195c8b79
21baki <53142091+21baki@users.noreply.github.com>
2020-01-05 18:03:07 +0100

    Create class Post (2)

--- model/Manager.php
+++ model/Manager.php
@@ -1,1 +1,10 @@
 <?php
+abstract class Manager {
+    protected $dbh;
+
+    public function __construct() { //Allows to call the db from the init
+        $this->dbh = new PDO('mysql:host=localhost;dbname=oc4', 'root', '');
+    }
+
+    public abstract function getEntityName();
+}
