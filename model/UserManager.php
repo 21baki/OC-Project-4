a748fa8d3843cf8ca4692b4f7b7bea62a3b3d13e
21baki <53142091+21baki@users.noreply.github.com>
2020-01-07 00:41:37 +0100

    construction of UserManager

--- model/UserManager.php
+++ model/UserManager.php
@@ -1,1 +1,29 @@
 <?php
+
+namespace OC4\Model;
+use PDO;
+
+class UserManager extends Manager {
+
+    //two functions that inherit from Manager
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getEntityName()
+    {
+        return "UserManager";
+    }
+
+    public function register($pseudo, $password, $email)
+    {
+        //TODO
+    }
+
+    public function login($pseudo, $password)
+    {
+        //TODO
+    }
+
+}
