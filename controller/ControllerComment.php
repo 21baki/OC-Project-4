11102fb3c1c276263de158b4f124e1128c2b3afd
21baki <53142091+21baki@users.noreply.github.com>
2020-01-29 10:52:23 +0100

    write createComment & editComment method of ControllerComment

--- controller/ControllerComment.php
+++ controller/ControllerComment.php
@@ -2,14 +2,40 @@
 
 class ControllerComment extends View
 {
-    public function createComment
+    public function createComment($request)
     {
-        //TODO
+        if(!$this->userSession->logged()) {
+            $this->redirect('connect');
+        }
+
+        $pseudo = $this->userSession->getPseudo();
+        $content = $request->get('content');
+        $postId = $request->get('id');
+
+        $manager = new CommentManager();
+        $manager->createComment($pseudo, $content, $postId);
+
+        $this->redirect('post/id/'.''.$postId);
     }
 
     public function editComment($request)
     {
-        //TODO
+        if($this->userSession->noRole('user') && $this->userSession->noRole('admin')) {
+            $this->redirect('home');
+        }
+
+        $id = $request->get('id');
+        $postId = $request->get('postId');
+
+        $manager = new CommentManager();
+        $comments = $manager->getComment($id);
+        $pseudo = $this->userSession->getPseudo();
+
+        if($pseudo != $comments->getAuthor() && $this->userSession->noRole('admin')) {
+            $this->redirect('403');
+        }
+
+        $this->render('editCom', array('comments' => $comments));
     }
 
     public function updateComment($request)
