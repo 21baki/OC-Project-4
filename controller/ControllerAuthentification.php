e99261ff1961f5ff3edd7dd558fa97844bb2e195
21baki <53142091+21baki@users.noreply.github.com>
2020-01-29 10:29:55 +0100

    write signin method

--- controller/ControllerAuthentification.php
+++ controller/ControllerAuthentification.php
@@ -2,12 +2,27 @@
 
 class ControllerAuthentification extends View
 {
-    public function signIn
+    public function signIn($request)
     {
-        //TODO
+        $manager = new UserManager();
+
+        $pseudo = $request->get('pseudo');
+        $password = $request->get('password');
+        $user = $manager->login($pseudo, $password);
+
+        if(isset($user) && password_verify($password, $user->getPassword())) {
+
+            $userSession = new UserSession();
+            $userSession->setPseudo($pseudo);
+            $userSession->setRole($user->getRole());
+            $this->redirect('home');
+        }
+
+        $user->setErrorsPseudo('Votre mot de passe ou votre pseudo est incorrect.');
+        $this->render('login', array('user' => $user));
     }
 
-    public function signOut
+    public function signOut()
     {
         //TODO
     }
