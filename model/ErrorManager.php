32156c114028f530b41067b3ad6d8f15e5ba3779
21baki <53142091+21baki@users.noreply.github.com>
2020-02-01 18:07:58 +0100

    write content of ErrorManager with regular expression

--- model/ErrorManager.php
+++ model/ErrorManager.php
@@ -9,6 +9,26 @@ class ErrorManager extends Manager
 
     public function verifyRegistration($pseudo, $password, $confirm, $email)
     {
-        //TODO
+        $errors = new Error();
+
+        if(!preg_match(' ^[a-zA-Z0-9_]{4,16}$^ ', $pseudo)) {
+            $errors->setErrorPseudo('Le pseudo choisi n\'est pas valide. Merci d\'en choisir un autre.');
+            $errors->setClean('x');
+        }
+
+        if(!preg_match(' ^[a-zA-Z0-9_]{5,16}$^ ', $password)) {
+            $errors->setErrorPassword('Le mot de passe choisi n\'est pas valide. Merci de le renforcer.');
+            $errors->setClean('x');
+        }
+
+        if($password != $confirm) {
+            $errors->setErrorConf('Ne correspond pas au mot de passe défini.');
+            $errors->setClean('x');
+        }
+
+        if(!preg_match(' #^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$# ', $email)) {
+            $errors->setErrorEmail('L\'adresse email choisie n\'est pas valide. Merci d\'en choisir une autre.');
+            $errors->setClean('x');
+        }
     }
 }
