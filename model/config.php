aa58722811e1cc876af8a9ec2e622e6684aa8097
21baki <53142091+21baki@users.noreply.github.com>
2020-02-02 11:36:02 +0100

    create & write config.php

---
+++ model/config.php
@@ -0,0 +1,43 @@
+<?php
+
+ini_set('display_errors', 'on');
+error_reporting(E_ALL);
+
+class Autoload
+{
+    public static function start()
+    {
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+
+        session_start();
+
+        $root = $_SERVER['DOCUMENT_ROOT'];
+        $host = $_SERVER['HTTP_HOST'];
+
+        define('HOST', 'http://'.$host.'/');
+        define('ROOT', $root.'/');
+
+        define('CONTROLLER', ROOT.'controller/');
+        define('MODEL', ROOT.'model/');
+        define('VIEW', ROOT.'view/');
+        define('CLASSE', ROOT.'class/');
+
+        define('PUBLIC', HOST.'public/');
+
+        define('DB_HOST', 'localhost');
+        define('DB_NAME', 'oc4');
+        define('DB_USERNAME', 'root');
+        define('DB_PASSWORD', 'PASSWORD');
+    }
+
+    public static function autoload($class)
+    {
+        if(file_exists(MODEL.$class.'.php')) {
+            include_once(MODEL.$class.'.php');
+        } elseif(file_exists(CLASSE.$class.'.php')) {
+            include_once(CLASSE.$class.'.php');
+        } elseif(file_exists(CONTROLLER.$class.'.php')) {
+           include_once(CONTROLLER.$class.'.php');
+        }
+    }
+}
