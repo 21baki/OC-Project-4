dc20d28df51b474d06230e84570c4a261bb546ab
21baki <53142091+21baki@users.noreply.github.com>
2020-02-02 12:01:24 +0100

    config Router.php

--- class/Router.php
+++ class/Router.php
@@ -6,13 +6,19 @@ class Router
     private $routes = [//TODO
      ];
 
-    public function __construct()
+    public function __construct($url)
     {
         $this->url = $url;
 
-        $route = $this->getRoute();
+        $route = $this->getRoutes();
+        $params = $this->getParams();
 
-        //TODO
+        $request = new Request();
+
+        $request->setRoute($route);
+        $request->setParams($params);
+
+        $this->request = $request;
     }
 
     /**
@@ -24,7 +30,42 @@ class Router
         return $elements[0];
     }
 
-    //TODO
+    public function getParams()
+    {
+        $params = array();
+        // extraction des paramètres de GET
+        $elements = explode('/', $this->url);
+        unset($elements[0]);
+        for($i = 1; $i<count($elements); $i++) {
+            $params[$elements[$i]] = $elements[$i+1];
+            $i++;
+        }
+        // extraction des paramètres de POST
+        if($_POST) {
+            foreach($_POST as $key => $value) {
+                $params[$key] = $value;
+            }
+        }
+        return $params;
+    }
+
+    public function renderController()
+    {
+        $request = $this->request;
+
+        if(key_exists($request->getRoutes(), $this->routes)) {
+            $controller = $this->routes[$request->getRoutes()]['controller'];
+            $method = $this->routes[$request->getRoutes()]['method'];
+            $area = $this->routes[$request->getRoutes()]['area'];
+
+            $currentController = new $controller();
+            $currentController->$method($request);
+
+        } else {
+            $newController = new View();
+            $newController->render('404');
+        }
+    }
 }
 
 
--- index.php
+++ index.php
@@ -9,30 +9,3 @@ Autoload::start();
 $router = new Router($request);
 $router->renderController();
 
-//TODO: replace author by pseudo in Post, Post Manager, Controller home, ect.. for more lisibility (and because i modified the database entry)
-
-
-
-
-
-
-/* require_once('controller/ControllerHome.php');
-var_dump($_GET);
-
-$url = '';
-if(isset($_GET['url'])) {
-    $url = explode('/', $_GET['url']);
-}
-
-var_dump($url);
-
-if ($url[0] == 'page' AND $url[1] == 'create_post') {
-    $post = new PostManager;
-    $post->createPost($_POST['title'], $_POST['content'], $_POST['author']);
-}else {
-    echo 'Erreur 404';
-} */
-
-
-
-//TODO: Take require() in autoloader
