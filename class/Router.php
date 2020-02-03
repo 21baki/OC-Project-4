7c0fba311d20fb345c31400bbd1f836d47da80ba
21baki <53142091+21baki@users.noreply.github.com>
2020-02-03 16:42:55 +0100

    write all routes of ControllerHome

--- class/Router.php
+++ class/Router.php
@@ -4,8 +4,17 @@ class Router
 {
     private $url;
     private $routes = [
-        "index"     => ["controller" => 'ControllerHome',         "method" => 'showHome',         "area" => ''],
-        "home"      => ["controller" => 'ControllerHome',         "method" => 'showHome',         "area" => ''],
+        "index"         => ["controller" => 'ControllerHome',         "method" => 'showHome',         "area" => ''],
+        "home"          => ["controller" => 'ControllerHome',         "method" => 'showHome',         "area" => ''],
+        "posts"         => ["controller" => 'ControllerHome',         "method" => 'showPosts',        "area" => ''],
+        "post"          => ["controller" => 'ControllerHome',         "method" => 'showPost',         "area" => ''],
+        "loginForm"     => ["controller" => 'ControllerHome',         "method" => 'showLogin',        "area" => ''],
+        "registerForm"  => ["controller" => 'ControllerHome',         "method" => 'showRegistration', "area" => ''],
+        "newPost"       => ["controller" => 'ControllerHome',         "method" => 'showEditForm',     "area" => ''],
+        "connect"       => ["controller" => 'ControllerHome',         "method" => 'showConnect',      "area" => ''],
+        "403"           => ["controller" => 'ControllerHome',         "method" => 'show403',          "area" => ''],
+        "404"           => ["controller" => 'ControllerHome',         "method" => 'show404',          "area" => ''],
+
      ];
 
     public function __construct($url)
