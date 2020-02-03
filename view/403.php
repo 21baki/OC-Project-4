ee36c4c1259a4c50128a9e21eec0d9f37523eb2e
21baki <53142091+21baki@users.noreply.github.com>
2020-02-03 22:30:50 +0100

    create & write 403 view

---
+++ view/403.php
@@ -0,0 +1,13 @@
+<div class="">
+    <div class="container container2">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-12 align-self-center">
+                <div class="">
+                    <h1>Erreur 403</h1>
+                    <h3>Vous n'avez pas l'autorisation d'accéder à cette page.</h3>
+                    <h3><a class="link" href="<?php echo HOST;?>home">Retourner à l'accueil</a></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
