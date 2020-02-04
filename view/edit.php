b804b7af0722e7cef2b30c966c8f65e08a7522d2
21baki <53142091+21baki@users.noreply.github.com>
2020-02-04 12:01:32 +0100

    complmete edit.php

--- view/edit.php
+++ view/edit.php
@@ -12,3 +12,35 @@
     $action = 'create';
 }
 ?>
+
+<div class="">
+    <div class="container container2">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-12">
+                <div class="contact-form">
+                    <h5>Editeur</h5>
+                    <form method="post" action="<?php echo HOST.''.$action.''.$id;?>">
+                        <fieldset>
+
+                            <div class="group">
+                                <input type="text" name="title" id="title" required>
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label><?php echo $title;?></label>
+                            </div>
+
+                            <div class="group">
+                                <textarea name="content" type="text" id="content" style="height: 500px;"><?php echo $content;?></textarea>
+                                <br />
+                                <span class="help-block" id="ErrNew" style="color:indigo;"></span>
+                            </div>
+                        </fieldset>
+
+                        <button id="submitButton" class="btn btn-primary" type="submit" value="">Envoyer</button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
