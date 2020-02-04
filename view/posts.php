a9369d3199c8f80729940101ac9293353a424f10
21baki <53142091+21baki@users.noreply.github.com>
2020-02-04 01:34:40 +0100

    create & write postS view

---
+++ view/posts.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 col-lg-12">
+            <div class="">
+                <h2> Tous les Articles/Chapitres </h2>
+                <?php foreach($Posts as $post);?>
+                <div class="container3">
+                    <a class="link" href="<?php echo HOST.'post/id/'.$post->getId()?>"><h3><?php echo $post->getTitle();?></h3></a>
+                    <br />
+                    <div class="PostContent"><?php echo substr($post->getContent(), 0, 1000);?>...<a class="link" href="<?php echo HOST.'post/id/'.$post->getId()?>">Suite</a> </div>
+                    <br />
+                    <span class="AuthorName"><?php echo $post->getAuthor();?></span>
+                    <span class="CreationDate"> <?php echo $post->getCreationDate()->format('d/m/Y');?> </span>
+                    <?php if($userSession->hasRole('admin')):?>
+                    <a class="link" href="<?php echo HOST.'edit/id/'.$post->getId()?>">Editer</a>
+                    <a class="link" href="<?php echo HOST.'delete/id/'.$post->getId()?>">Supprimer</a>
+                    <?php endif;?>
+                    <?php if($userSession->logged()):?>
+                    <a class="link" href="<?php echo HOST.'post/id/'.$post->getId();?>">Commenter</a>
+                    <?php endif;?>
+                </div>
+                <?php endforeach;?>
+            </div>
+        </div>
+    </div>
+</div>
