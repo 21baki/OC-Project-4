71550c26680291677348c8c06cca29cd62aa2acd
21baki <53142091+21baki@users.noreply.github.com>
2020-02-04 17:43:09 +0100

    complete register.php

--- view/register.php
+++ view/register.php
@@ -14,11 +14,69 @@
     $errorsPseudo = '';
     $errorsMail = '';
 
-    $ErrPseudo = '';
-    $ErrPass = '';
-    $ErrConf = '';
-    $ErrEmail = '';
+    $ErrorPseudo = '';
+    $ErrorPass = '';
+    $ErrorConf = '';
+    $ErrorEmail = '';
 }
 ?>
 
+<div class="">
+    <div class="container container2">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-12">
+                <div class="contact-form">
+                    <h5>Formulaire d'inscription</h5>
+                    <form class="form-horizontal" method="post" action="register">
+                        <fieldset>
 
+                            <div class="group" style="max-width: 50%;">
+                                <span class="help-block" style="color: indigo;"><?php echo $errorsPseudo;?></span>
+                                <input type="text" name="pseudo" id="pseudo" value="<?php echo $pseudo;?>" required/>
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label for="pseudo">Pseudo</label>
+                                <span class="help-block">4 à 16 caractères, lettres et/ou chiffres</span>
+                                <span class="help-block" style="color:indigo;"><?php echo $ErrorPseudo?></span>
+                            </div>
+
+                            <div class="group" style="max-width: 50%;">
+                                <input type="password" name="password" id="password"  value="" required>
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label for="password">Mot de passe</label>
+                                <span class="help-block">5 à 16 caractères, lettres et chiffres, caractères spéciaux</span>
+                                <span class="help-block" style="color:indigo;"><?php echo $ErrorPass?></span>
+                            </div>
+
+                            <div class="group" style="max-width: 50%;">
+                                <input type="password" name="confirm" id="confirm"  value="" required>
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label for="confirm">Confirmation du mot de passe</label>
+                                <span class="help-block" style="color:indigo;"><?php echo $ErrorConf;?></span>
+                            </div>
+
+                            <div class="group" style="max-width: 50%">
+                                <span class="help-block" style="color: indigo"><?php echo $errorsMail;?></span>
+                                <input type="text" name="email" id="email"  value="<?php echo $email;?>" required>
+                                <span class="highlight"></span>
+                                <span class="bar"></span>
+                                <label for="email">E-mail</label>
+                                <span class="help-block" style="color:indigo;"><?php echo $ErrEmail;?></span>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label" for="loginButton"></label>
+                                <div class="col-sm-4">
+                                    <button id="loginButton" name="loginButton" class="btn btn-primary">Inscription</button>
+                                </div>
+                            </div>
+
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
