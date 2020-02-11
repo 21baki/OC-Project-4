4b96562a6ed40967e55ae4080a0528efd46ac85e
21baki <53142091+21baki@users.noreply.github.com>
2020-02-11 13:43:43 +0100

    change all regex (ErroManager)

--- model/ErrorManager.php
+++ model/ErrorManager.php
@@ -11,13 +11,13 @@ class ErrorManager extends Manager
     {
         $errors = new Error();
 
-        if(!preg_match(' ^[a-zA-Z0-9_]{4,16}$^ ', $pseudo)) {
+        if(!preg_match(' ^[a-zA-Z0-9_]{3,16}$\ ^ ', $pseudo)) {
             $errors->setErrorPseudo('Le pseudo choisi n\'est pas valide. Merci d\'en choisir un autre.');
             $errors->setClean('x');
         }
 
         //TODO: Hasher le mot de passe
-        if(!preg_match(' ^[a-zA-Z0-9_]{5,16}$^ ', $password)) {
+        if(!preg_match(' ^[a-zA-Z0-9_]{6,16}$\ ^ ', $password)) {
             $errors->setErrorPassword('Le mot de passe choisi n\'est pas valide. Merci de le renforcer.');
             $errors->setClean('x');
         }
@@ -27,12 +27,15 @@ class ErrorManager extends Manager
             $errors->setClean('x');
         }
         //strip tags à la place de preg_match
-        if(!preg_match(' /^.+@.+\.[a-zA-Z]{2,}$/ ', $email)) {
+        if(!preg_match(' ^.+@.+\.[a-zA-Z]{2,}$\ ^ ', $email)) {
             $errors->setErrorEmail('L\'adresse email choisie n\'est pas valide. Merci d\'en choisir une autre.');
             $errors->setClean('x');
         }
 
-        var_dump(!preg_match(' ^[a-zA-Z0-9_]{4,16}$^ ', $pseudo));
+        var_dump(!preg_match(' ^[a-zA-Z0-9_]{3,16}$\ ^ ', $pseudo));
+        var_dump(!preg_match(' ^[a-zA-Z0-9_]{3,16}$\ ^ ', $password));
+        var_dump(!preg_match(' ^.+@.+\.[a-zA-Z]{2,}$\ ^ ', $email));
+
 
         return $errors;
     }
