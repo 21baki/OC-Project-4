1d13b9745e619dbc6b0d0316d4653e1f7dd1e50e
21baki <53142091+21baki@users.noreply.github.com>
2020-02-24 15:13:22 +0100

    add required for register input

--- view/register.php
+++ view/register.php
@@ -24,13 +24,13 @@
 
                                 <div class="form-group">
                                     <label for="pseudo">Pseudo</label>
-                                    <input type="text" name="pseudo" id="pseudo" value="<?= htmlspecialchars($pseudo);?>" class="form-control"/>
+                                    <input type="text" name="pseudo" id="pseudo" value="<?= htmlspecialchars($pseudo);?>" class="form-control" required/>
                                     <small class="form-text text-muted">3 à 10 caractères.</small>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="password1">Mot de passe</label>
-                                    <input type="password" name="password" id="password1"  value="<?php echo htmlspecialchars($password);?>" class="form-control">
+                                    <input type="password" name="password" id="password1"  value="<?php echo htmlspecialchars($password);?>" class="form-control" required/>
                                     <small class="form-text text-muted">5 à 16 caractères. </small>
                                 </div>
 
@@ -40,13 +40,13 @@
 
                                 <div class="form-group">
                                     <label for="email">E-mail</label>
-                                    <input type="text" name="email" id="email"  value="<?php echo htmlspecialchars($email);?>" class="form-control">
+                                    <input type="text" name="email" id="email"  value="<?php echo htmlspecialchars($email);?>" class="form-control" required/>
                                     <small class="form-text text-muted">Nous ne partagerons votre e-mail avec personne.</small>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="confirm">Confirmation du mot de passe</label>
-                                    <input type="password" name="confirm" id="confirm"  value="<?php echo htmlspecialchars($confirm);?>" class="form-control">
+                                    <input type="password" name="confirm" id="confirm"  value="<?php echo htmlspecialchars($confirm);?>" class="form-control" required/>
                                     <small class="form-text text-muted">Assurez-vous de ne pas avoir fait de fautes de frappe.</small>
                                 </div>
                             </div>
