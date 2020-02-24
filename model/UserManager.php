4020b39f1bb01f994e6ef845809d272e1ea29f21
21baki <53142091+21baki@users.noreply.github.com>
2020-02-24 14:53:00 +0100

    remove todo & inutile method

--- model/UserManager.php
+++ model/UserManager.php
@@ -30,44 +30,8 @@ class UserManager extends Manager {
         $req->execute();
     }
 
-    /* public function login($pseudo, $password)
-    {
-        $dbh = $this->dbh;
-
-        $query = 'SELECT * FROM users WHERE pseudo = :pseudo AND password = :password';
-
-        $req = $dbh->prepare($query);
-        $req->bindParam('pseudo', $pseudo, PDO::PARAM_STR);
-        $req->bindParam('password', $password, PDO::PARAM_STR);
-
-        $req->execute();
-
-
-        //La requête s'exécute bien
-        //var_dump($req->execute());
-
-
-        $user = new User();
-        $data = $req->fetch();
-        var_dump($data);
-
-        if ($data && !empty($data)) { // Verif si fetch() est bien un tableau et si il ne retourne pas vide
-
-            $user->hydrate($data);
-            return $user;
-        } else {
-
-            //$user->hydrate($user);
-            return $user;
-        }
-
-
-
-        //return $user;
-    } */
-
     public function checkPseudoForLogin($pseudo)
-{
+    {
     $dbh = $this->dbh;
 
     $query = 'SELECT * FROM users WHERE pseudo = :pseudo';
@@ -81,8 +45,7 @@ class UserManager extends Manager {
     $user->hydrate($data);
 
     return $user;
-
-}
+    }
 
     public function verify($pseudo, $email)
     {
@@ -110,7 +73,7 @@ class UserManager extends Manager {
                 $user->setErrorsPseudo('Le pseudo est déja pris. Merci d\'en choisir un autre.');
             }
             if($user->getEmail() === $email) {
-                        //TODO
+
                 $user->setErrorsMail('L\'email est déja pris. Merci d\'en choisir un autre.');
             }
 
